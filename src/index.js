7121963c6b4a921f9b89ec5b97606bacc705771a
Sedyshev serge <zxlive@yandex.ru>
2021-09-08 20:06:59 +0400

    jk

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from  'react';
 import ReactDOM  from 'react-dom';
 
-const el= React.createElement(
-  'h1',
-  null,
-  'Hello Word, React.js!'
-);
+// const el= React.createElement(
+//   'h1',
+//   null,
+//   'Hello Word, React.js!'
+// );
+
+const el =( <React.Fragment><h1> Hello Word</h1>
+  <ul>
+    <li>
+      Первый
+    </li>
+    <li>
+      Второй
+    </li>
+  </ul></React.Fragment>
+  );
+console.log(el);
+
 
 ReactDOM.render(el, document.getElementById('root'));
