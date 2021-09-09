f2bc3d118f5b3bc739b78e74532c850c1d784e30
Sedyshev Serge <zxlive@yandex.ru>
2021-09-09 10:10:34 +0400

    ds

--- src/components/HeaderBlock/index.js
+++ src/components/HeaderBlock/index.js
@@ -1,3 +1,14 @@
+import './style.css';
+
 const HeaderBlock =()=>{
-	
+	return (
+		<div>
+			<div>
+				<h1> This pokemon card game</h1>
+				<p>Simple Triple Triad Game!</p>
+			</div>
+
+		</div>
+
+	)
 }
