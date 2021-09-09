a732d5a8b3e4b71aed8ecb2400747a7dec4ddc1f
Sedyshev Serge <zxlive@yandex.ru>
2021-09-09 11:16:31 +0400

    ds

--- src/components/HeaderBlock/index.js
+++ src/components/HeaderBlock/index.js
@@ -2,11 +2,11 @@ import s from './style.module.css';
 // console.log('#### s', s);
 
 const HeaderBlock =({title,hideBackground = false})=>{
-
+	const styleRoot= hideBackground ?{backgroundImage:'none'}:{};
 	return (
 		<div>
 			<div>
-				<h1 className={s.header}>
+				<h1 className={s.header} style={styleRoot}>
 					 {title}
 					 </h1>
 				<p>Simple Triple Triad Game!</p>
